1d5b69c1f5d699bb1df3adb0764a62ba39ab7656
darkstars77 <darkstars77@naver.com>
2022-09-20 22:05:10 +0900

    add maker template

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router";
-import Footer from "../footer/footer";
-import Header from "../header/header";
-import styles from "./login.module.css";
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import styles from './login.module.css';
 
 const Login = ({ authService }) => {
   const history = useHistory();
-  const goToMaker = (userId) => {
+  const goToMaker = userId => {
     history.push({
-      pathname: "/maker",
+      pathname: '/maker',
       state: { id: userId },
     });
   };
 
-  const onLogin = (event) => {
+  const onLogin = event => {
     authService //
       .login(event.currentTarget.textContent)
-      .then((data) => goToMaker(data.user.uid));
+      .then(data => goToMaker(data.user.uid));
   };
 
-  useEffect(()=>{
-    authService.onAuthChange(user =>{
-      user && goToMaker(user.uid);
+  useEffect(() => {
+    authService.onAuthChange(user => {
+      user && goToMaker(user.id);
     });
   });
 
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
-import { useHistory } from "react-router";
-import Footer from "../footer/footer";
-import Header from "../header/header";
-import styles from "./maker.module.css";
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
+import styles from './maker.module.css';
+
 const Maker = ({ authService }) => {
   const history = useHistory();
   const onLogout = () => {
@@ -10,16 +13,19 @@ const Maker = ({ authService }) => {
   };
 
   useEffect(() => {
-    authService.onAuthChange((user) => {
+    authService.onAuthChange(user => {
       if (!user) {
         history.push('/');
       }
     });
   });
-
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
